cf0f491373cab56a4924a684af3d0b012b346de3
Felipe Martins <felipe_mmmachado@hotmail.com>
2022-07-28 09:55:53 -0300

    Improving User Experience

--- main.py
+++ main.py
@@ -1,88 +1,35 @@
-import random
+from hangman_words import word_list
+from hangman_art import *
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import random
+import os
 
 #Step 1 
-
-word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 chosen_word = list(chosen_word)
-print(chosen_word)
 traces = []
 tela = ''
+
+print(logo)
 
 for n in chosen_word:
     traces.append('_')
 
+print()
+print(' '.join(traces))
 traces = ''.join(traces)
-
-print(f"\n{traces}")  
+ 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 lives = 6
 
 while lives != 0:
     print(stages[lives])
-    print(tela)
     guess = str(input("Guess a letter: ").lower())
     
+    os.system('cls')
     #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
     traces = list(traces)
     checkTraces = len(traces)
@@ -92,21 +39,24 @@ while lives != 0:
         letter = chosen_word[n]
         if letter == guess:
           traces[n] = guess
-          tela = ''.join(traces)
+          tela = ' '.join(traces)
 
         elif checkError == 0:
           lives -= 1
-          checkError = 1
+          checkError = 1  
 
         checkTraces = traces.count('_')
         
         if checkTraces == 0:
+          print(tela)
           print("The player won!")
           exit()
 
         elif lives == 0:
-          print(stages[0])
+          print(stages[lives])
+          print("the word was: " + ''.join(chosen_word))
           print("You lose!")
           exit()  
 
+    print(tela)
     
